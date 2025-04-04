aa0396056031542f042733dceb5a05784d602695
Nshuti7 <bricekabanda8@gmail.com>
2025-04-04 19:07:41 +0300

    Commit3

---
+++ ExchangeRate/src/test/java/ExchangeRate_Package/ExchangeRateTest.java
@@ -0,0 +1,20 @@
+package ExchangeRate_Package;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ExchangeRateTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getExchangeRate() {
+    }
+
+    @Test
+    void setExchangeRate() {
+    }
+}
